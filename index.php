7b4b3c6fc244c2789bbe384de9760e0b29631aca
Umair Malik <umair6336@gmail.com>
2021-03-26 12:47:46 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,6 @@ if (!empty($_GET['url'])) {
     //get data for URL
     $data = url_get_contents($_GET['url']);
 
-
-
-
-
     //get the video links and title
     $hdlink = hdLink($data);
     $sdlink = sdLink($data);
@@ -19,49 +15,16 @@ if (!empty($_GET['url'])) {
     $title = getTitle($data);
 
 
-
-
-        $hd = curl_init($hdlink);
-
-        curl_setopt($hd, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($hd, CURLOPT_HEADER, TRUE);
-        curl_setopt($hd, CURLOPT_NOBODY, TRUE);
-
-        $data = curl_exec($hd);
-        $hdsize = curl_getinfo($hd, CURLINFO_CONTENT_LENGTH_DOWNLOAD);       
-
-        curl_close($hd);
-
-        $sd = curl_init($sdlink);
-
-        curl_setopt($sd, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($sd, CURLOPT_HEADER, TRUE);
-        curl_setopt($sd, CURLOPT_NOBODY, TRUE);
-
-        $data = curl_exec($sd);
-        $sdsize = curl_getinfo($sd, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-        curl_close($sd);
-
     if ($sdlink != "" && $hdlink != "" ) 
     {
         //Get both the SD and HD video quality
         //$download_button = '<a target="_blank" href="'.$sdlink.'" class="btn btn-lg btn-block btn-success"> (MP4) Download SD Video</a>';
         //$download_button .= '<a style="top:70%" target="_blank" href="'.$hdlink.'" class="btn btn-block btn-lg btn-success"> (MP4) Download HD Video</a>';
-        
-        
-        
-        
-        
-        
-        
-        
         echo json_encode( array(
             'hdlink' => str_replace('\/','/',$hdlink),
-            'hdsize' => "$hdsize",
-            'sdlink' => $sdlink,
-            'sdsize' => "$sdsize",
+            'sdlink' => sdLink($data),
             'mp3link' => mp3Link($data),
-            'title' => $title
+            'title' => getTitle($data)
             )
         );
     }
@@ -72,12 +35,10 @@ if (!empty($_GET['url'])) {
         //$download_button = '<a target="_blank" href="'.$sdlink.'" class="btn btn-lg btn-block btn-success"> (MP4) Download SD Video</a>';
 
         echo json_encode( array(
-            'hdlink' => null,
-            'hdsize' => "$hdsize",
-            'sdlink' => $sdlink,
-            'sdsize' => "$sdsize",
+            'hdlink' => hdLink($data),
+            'sdlink' => sdLink($data),
             'mp3link' => mp3Link($data),
-            'title' => $title
+            'title' => getTitle($data)
             )
         );
 
@@ -86,12 +47,10 @@ if (!empty($_GET['url'])) {
     //Get the HD video alone
     // $download_button = '<a target="_blank" href="'.$hdlink.'" class="btn btn-lg btn-block btn-success"> (MP4) Download HD Video</a>';
         echo json_encode( array(
-        'hdlink' => str_replace('\/','/',$hdlink),
-            'hdsize' => "$hdsize",
-            'sdlink' => null,
-            'sdsize' => "$sdsize",
-            'mp3link' => mp3Link($data),
-            'title' => $title
+        'hdlink' => hdLink($data),
+        'sdlink' => sdLink($data),
+        'mp3link' => mp3Link($data),
+        'title' => getTitle($data)
         )
     );
 
